d142a488f1d1e684e44540608f91df147d36ca66
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-14 22:04:48 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -20,7 +20,8 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-  
+end
+shipping_manifest["oil paintings"]
 end
 
 def adding
